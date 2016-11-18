6a8440b0345d4d00fdb7870b647d51b8c8207c75
Wapika <mattldwig@gmail.com>
2016-11-18 14:35:27 +0100

    Finalisation fonctions de base et ajout score

--- js/app.js
+++ js/app.js
@@ -1,47 +1,64 @@
-// Enemies our player must avoid
 var Enemy = function() {
     this.reset();
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x = this.speed + this.x;
-
+    this.x = this.speed * dt + this.x;
     if(this.x > 500) {
       this.reset();
     }
+    if (this.x > -20){
+      this.checkCollisions();
+    }
 };
+
+var board = function(){}
+
+board.reset = function() {
+  player.reset();
+  for(var i = 0 ; i < allEnemies.length ; i++) {
+    allEnemies[i].reset();
+  }
+  player.score = 0;
+}
 
 Enemy.prototype.reset = function() {
+  this.width = 98;
+  this.height = 68;
   this.col = -1;
-  this.row = randomize(1,3);
+  this.row = randomize(1,4);
   this.x = 101 * this.col;
   this.y = 83 * this.row;
-  this.speed = randomize(1,5);
+  this.speed = randomize(20,100);
 };
 
-// Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
 var Player = function(){
   this.reset();
   this.sprite = 'images/char-boy.png';
 };
 
 Player.prototype.reset = function(){
+  this.width = 68;
+  this.height = 76;
   this.col = 4;
   this.row = 5;
   this.movable = true;
+}
+
+Enemy.prototype.checkCollisions = function() {
+  if(((player.y + player.height) < (this.y)) ||
+      (player.y > (this.y + this.height)) ||
+      ((player.x + player.width) < this.x) ||
+      (player.x > (this.x + this.width))) {
+      }
+      else {
+        board.reset();
+      }
 }
 
 Player.prototype.handleInput = function(key) {
@@ -72,6 +89,7 @@ Player.prototype.update = function(dt) {
   }
   if(this.y < 83 && this.movable) {
     this.movable = false;
+    this.score += 1;
     player.reset();
     return true;
   }
@@ -82,35 +100,29 @@ Player.prototype.render = function() {
   ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
+Player.prototype.score = function() {
+  this.score = 0;
+}
+
 function randomize(min,max){
   return  Math.floor(Math.random() * (max - min)) + min;
 }
 
-function checkCollisions(){
-  for(var i = 0 ; i < allEnemies.length ; i++){
-      if((player.x && player.y) === allEnemies[i].x && allEnemies[i].y){
-        player.reset();
-        allEnemies[i].reset();
-      }
-  }
-}
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 var allEnemies = [];
 
 var enemie0 = new Enemy();
 var enemie1 = new Enemy();
 var enemie2 = new Enemy();
-for(i=0; i < 3 ; i++){
+var enemie3 = new Enemy();
+var enemie4 = new Enemy();
+for(i=0; i < 6 ; i++){
   allEnemies.push(new Enemy());
 }
 
 
 var player = new Player();
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
